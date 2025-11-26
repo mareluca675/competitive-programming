4567266d9ce52b31844660baa13536a3e30b8d3d
Mare Luca <marelucaghita@gmail.com>
2025-11-26 21:01:38 +0200

    Dynamic programming and dijkstra.

---
+++ OJI/XI/2015/dragoni.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+
+const int INF = INT_MAX / 2;
+
+std::set<std::tuple<int, int, int>> q;
+std::vector<std::vector<int>> dist;
+std::vector<std::vector<std::pair<int, int>>> graph;
+std::vector<int> drag;
+
+int main() {
+    std::freopen("dragoni.in", "r", stdin);
+    std::freopen("dragoni.out", "w", stdout);
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    int n, m;
+    std::cin >> n >> m;
+
+    drag.resize(n + 1);
+    for(int i = 1; i <= n; ++i) {
+        std::cin >> drag[i];
+    }
+
+    graph.resize(n + 1); 
+
+    for(int i = 1; i <= m; ++i) {
+        int from, to, distance;
+        std::cin >> from >> to >> distance;
+        graph[from].push_back(std::make_pair(to, distance));
+        graph[to].push_back(std::make_pair(from, distance));
+    }
+
+    if(t == 1) {
+        std::queue<int> q;
+        q.push(1);
+
+        int maximum = drag[1];
+       
+        std::vector<bool> visited(n + 1);
+        visited[1] = true;
+
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for(auto [to, distance] : graph[node]) {
+                if(distance <= drag[1] && !visited[to]) {
+                    q.push(to);
+                    maximum = std::max(maximum, drag[to]);
+                    visited[to] = true;
+                }
+            }
+        }
+
+        std::cout << maximum;
+    }
+    else if(t == 2) {
+        q.insert(std::make_tuple(0, 1, 1));
+        
+        dist.resize(n + 1, std::vector<int>(n + 1, INF));
+        dist[1][1] = 0;
+
+        while(!q.empty()) {
+            auto [d, from, current] = *q.begin();
+            q.erase(q.begin());
+
+            if(from == n) {
+                std::cout <<  d;
+                return 0;
+            }
+
+            if(d > dist[from][current]) continue;
+
+            // Am ajuns cu un alt dragon intr-un timp mai bun?
+            if(dist[from][from] > dist[from][current]) {
+
+                // Stergem distanta veche
+                if(dist[from][from] != INF) {
+                    q.erase(q.find(std::make_tuple(dist[from][from], from, from)));
+                }
+
+                // O adaptam, dar pastram dragonul diferit
+                dist[from][from] = dist[from][current];
+                q.insert(std::make_tuple(dist[from][from], from, from));
+            } 
+
+            for(auto [to, cost] : graph[from]) {
+                if(dist[from][current] + cost < dist[to][current] && drag[current] >= cost) {
+                    if(dist[to][current] != INF) {
+                        q.erase(q.find(std::make_tuple(dist[to][current], to, current)));
+                    }
+
+                    dist[to][current] = dist[from][current] + cost;
+                    q.insert(std::make_tuple(dist[to][current], to, current));
+                }
+            }
+        }
+    }
+
+    return 0;
+}
--- codeforces/round-448-div2/C.cpp
+++ codeforces/round-448-div2/C.cpp
@@ -1,7 +1,42 @@
 #include <bits/stdc++.h>
 
 const int MOD = 1e9 + 7;
-const int MAX_VALUE = 70;
+const int MAX_NUMBER = 71;
+const int MAX_PRIMES = 19;
+
+int primes[MAX_PRIMES] = {
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67
+};
+
+std::vector<std::vector<int>> dp;
+std::vector<int> state;
+std::vector<int> f;
+std::vector<int> pow2;
+
+void compute_powers_of_two(int limit) {
+    pow2.resize(limit + 1);
+    pow2[0] = 1;
+
+    for(int i = 1; i <= limit; ++i)
+        pow2[i] = (pow2[i - 1] << 1) % MOD;
+}
+
+void compute_states(std::vector<int> f, std::vector<int>& state) {
+    for (int i = 1; i < MAX_NUMBER; ++i) {
+        if (f[i]) {
+            int current = i;
+
+            for (int j = 0; j < MAX_PRIMES && primes[j] <= current; ++j) {
+                while(current != 1 && current % primes[j] == 0) {
+                    state[i] ^= (1 << j);
+                    current /= primes[j];
+
+                    // std::cout << i << ' ' << states[i] << '\n';
+                }
+            }
+        }
+    }
+}
 
 int main() {
     std::ios_base::sync_with_stdio(false);
@@ -11,12 +46,47 @@ int main() {
     int n;
     std::cin >> n;
 
-    std::vector<int> f(MAX_VALUE, 0);
-    for(int i = 1; i <= n; ++i) {
+    f.resize(MAX_NUMBER, 0);
+
+    for (int i = 0; i < n; ++i) {
         int x;
         std::cin >> x;
         ++f[x];
     }
 
+    state.resize(MAX_NUMBER, 0);
+    compute_states(f, state);
+    compute_powers_of_two(n);
+
+    // Numarul de submultimi folosind valori mai mici ca [i] 
+    // care au starea [bitMask]
+
+    // Cazul 1: folosim un numar par de numere de [i],
+    // astfel putem lua submultimile de dinainte, fiindca nu
+    // adaugam factori primi la putere impara
     
+    // Cazul 2: folosim un numar impar de numere de [i],
+    // astfel putem lua doar submultimile care au si ele
+    // aceiasi factori primi la putere impara, pentru ca
+    // produsul final sa ii aiba la putere para
+    dp.resize(MAX_NUMBER, std::vector<int>(1 << MAX_PRIMES, 0));
+
+    dp[0][0] = 1;
+
+    for(int i = 1; i < MAX_NUMBER; ++i) {
+        for(int bitMask = 0; bitMask < (1 << MAX_PRIMES); ++bitMask) {
+            if(f[i]) {
+                dp[i][bitMask] = (
+                    ((1LL * dp[i - 1][bitMask] % MOD * pow2[f[i] - 1] % MOD) % MOD +
+                     (1LL * dp[i - 1][bitMask ^ state[i]] % MOD * pow2[f[i] - 1] % MOD) % MOD) % MOD
+                );
+            }
+            else {
+                dp[i][bitMask] = dp[i - 1][bitMask];
+            }
+        }
+    }
+
+    std::cout << (dp[MAX_NUMBER - 1][0] - 1 + MOD) % MOD;
+    return 0;
 }
